b2de031ca4a154a070d22600c8493296bd5579b6
janinene <janinem.usana@gmail.com>
2022-09-27 09:31:14 -0400

    create a multi-module spring project. Closes #1

---
+++ PetClinic/pet-clinic-web/src/main/java/petclinic/model/Pet.java
@@ -0,0 +1,35 @@
+package petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    private PetType petType;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    private Owner owner;
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    private LocalDate birthdate;
+}
---
+++ PetClinic/pet-clinic-web/src/main/java/petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package petclinic.model;
+
+public class PetType {
+    public String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
